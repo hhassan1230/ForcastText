c7d52a5e0eb6250839b00719eba13dae4fa49e7f
hhassan1230 <hhassan@oswego.edu>
2015-09-23 00:29:56 -0400

    working reading weather api

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,24 +1,26 @@
-require 'open-uri'
 class Forecast
+
 	attr_reader :ip, :location, :hash_weather
 
 	def initialize(ip=nil, location={})
 		@ip = ip
 		@location = location
-		w_hash = hash_weather(api_call(@location))
+		hash_weather
+	end
+
+	def api_call
 		binding.pry
+		@the_call = "https://api.forecast.io/forecast/#{ENV["APIKEY"]}/#{location[:latitude]},#{location[:longitude]}"
 	end
 
-	def api_call(location)
-		"https://api.forecast.io/forecast/#{ENV["APIKEY"]}/#{location[:latitude]},#{location[:longitude]}"
-	end
-
-	def hash_weather(the_call)
-		file = open(the_call)
+	def hash_weather
+		file = open(api_call)
 		read = File.read(file)
 		@hash_weather = JSON.parse(read)
+		binding.pry
 		@hash_weather 
+		
 	end
 
-	
+
 end
